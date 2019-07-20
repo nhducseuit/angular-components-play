bfbf49be178c26252e81a4eb91d4d462d67dabe2
Duc Nguyen Hoang <nhduc.seuit@gmail.com>
2019-07-20 19:19:41 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-components-play';
+}
